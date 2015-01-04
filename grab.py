4c33e00391ccdf074c08c803deefc6fdc0cf4c83
lucasw <lucasw_backwards@gmail.com>
2015-01-04 11:45:33 -0800

    Grab images of Rainier from SDOT once a minute.

---
+++ grab.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#import wget
+#import datetime 
+import urllib2
+import cStringIO
+import cv2
+import time
+
+url_base = 'http://www.seattle.gov/trafficcams/images/'
+cam_list = [ \
+    'Rainier_Henderson',
+    'Rainier_Hudson',
+    'MLK_Rainier', 
+    'Rainier_MLK',
+    ]
+postfix = '.jpg'
+
+start_time = time.strftime("%Y%m%d_%H%M%S") # str(datetime.datetime.now().time())
+print 'start time', start_time
+
+count = 10000000
+while True:
+    
+    for ind, cam in enumerate(cam_list):
+        url = url_base + cam + postfix
+        try: 
+            image_file = cStringIO.StringIO(urllib2.urlopen(url).read())
+        except:
+            continue
+            
+        name = 'data/' + cam + '_' + start_time + "_" + str(count) + postfix
+        with open(name, 'w') as fh:
+            fh.write(image_file.getvalue())
+        #im = cv2.imread(image_file)
+        #cv2.imshow(cam, image_file)
+
+    count += 1
+    # they don't update more than once a minute, or are otherwise preventing the same
+    # requester from asking for them more often
+    time.sleep(60.0)
+    #cv2.waitKey(0)
+
